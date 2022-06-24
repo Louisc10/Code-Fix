79b03cc03a7b9183239787f8bd7d696a36331387
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2022-06-24 23:03:55 +0700

    Case Pert1

---
+++ Soal/Case 1-2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	char word[] = "Welcome to CodeFix";
+	
+	printf(word);
+}
+
+/*
+#include<stdio.h>
+
+int main(){
+	char word[] = "Welcome to CodeFix";
+	
+
+}
+*/
