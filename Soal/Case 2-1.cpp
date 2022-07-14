3932200e7e2091fb6fffca5e97c2672b395db803
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2022-07-14 11:52:31 +0700

    update gitignore for program and add new question

---
+++ Soal/Case 2-1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("Total sum is %d", a + b);
+	
+	return 0;
+}
+
+/*
+#include<stdio.h>
+
+int main(){
+	int a, b;
+	scanf("%d %d", , );
+	printf("Total sum is %d",   );
+	
+	return 0;
+}
+*/
