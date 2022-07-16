3e6d906b05c3c7b5119d431be654dccd1bd92895
zzeordyga <47037524+zzeordyga@users.noreply.github.com>
2022-07-16 22:25:47 +0700

    adding drag and drop, snapping still has errors

---
+++ App/CodeFix/Assets/Scripts/DragController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+    public Draggable LastDragged => lastDragged;
+
+    // Check if user is dragging
+    private bool isDragActive = false;
+
+    // Check position of input
+    private Vector2 screenPosition;
+    private Vector3 worldPosition;
+
+    // Get last dragged item
+    private Draggable lastDragged;
+
+    void Awake()
+    {
+        DragController[] controllers = FindObjectsOfType<DragController>();
+
+        if(controllers.Length > 1)
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDragActive && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)))
+        {
+            Drop();
+            return;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            screenPosition = new Vector2(mousePos.x, mousePos.y);
+        }
+        else if (Input.touchCount > 0)
+        {
+            screenPosition = Input.GetTouch(0).position;
+        }
+        else
+        {
+            return;
+        }
+
+        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        if (isDragActive)
+        {
+            Drag();
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+            if(hit.collider != null)
+            {
+                Draggable draggable = hit.transform.gameObject.GetComponent<Draggable>();
+                if(draggable != null)
+                {
+                    lastDragged = draggable;
+                    InitDrag();
+                }
+            }
+        }
+    }
+
+    void InitDrag()
+    {
+        lastDragged.LastPosition = lastDragged.transform.position;
+        UpdateDragStatus(true);
+    }
+
+    void Drag()
+    {
+        lastDragged.transform.position = new Vector2(worldPosition.x, worldPosition.y);
+    }
+
+    void Drop()
+    {
+        UpdateDragStatus(false);
+    }
+
+    void UpdateDragStatus (bool isDragging)
+    {
+        isDragActive = lastDragged.IsDragging = isDragging;
+        lastDragged.gameObject.layer = isDragging ? Layers.Dragging : Layers.Default;
+    }
+}
---
+++ App/CodeFix/Assets/Scripts/Draggable.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    public bool IsDragging;
+
+    public Vector3 LastPosition;
+
+    private Collider2D _collider;
+
+    private DragController dragController;
+
+    private float movementTime = 15f;
+    private System.Nullable<Vector3> movementDst;
+
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        dragController = FindObjectOfType<DragController>();
+    }
+
+    void FixedUpdate()
+    {
+        if(movementDst.HasValue)
+        {
+            if (IsDragging)
+            {
+                movementDst = null;
+                return;
+            }
+
+            if(transform.position == movementDst)
+            {
+                gameObject.layer = Layers.Default;
+                movementDst = null;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, movementDst.Value, movementTime * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Draggable collideDraggable = other.GetComponent<Draggable>();
+
+        if(collideDraggable != null && dragController.LastDragged?.gameObject == gameObject)
+        {
+            ColliderDistance2D colliderDistance2D = other.Distance(_collider);
+            Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y);
+
+            // Distance not applied for some reason
+            Debug.Log(diff);
+
+            transform.position -= diff;
+        }
+
+        if (other.CompareTag("DropValid"))
+        {
+            movementDst = other.transform.position;
+        }
+        else if (other.CompareTag("DropInvalid"))
+        {
+            movementDst = LastPosition;
+        }
+    }
+}
