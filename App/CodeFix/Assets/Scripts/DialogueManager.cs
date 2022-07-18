59196b122a1e92c8f6ff07f8138498229d9ab3f0
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2022-07-18 08:46:24 +0700

    Add Dialog Manager
    You can check at Dialog Scene

---
+++ App/CodeFix/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TMP_Text dialogueText;
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialog(){
+        animator.SetBool("IsOpen", false);
+        dialogueText.text = "End Of Dialog";
+    }
+}
