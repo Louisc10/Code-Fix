c38faa9bc1b98eb7ad78379764aa3218847cfa1c
zzeordyga <zzeordyga@gmail.com>
2022-07-18 18:20:46 +0700

    adding java questions and fixing directory for c questions

---
+++ App/CodeFix/Assets/Questions/Level/Java/Level4OddEvenDetection.java
@@ -0,0 +1,20 @@
+package levels;
+
+/**
+ * Answers
+ * %, !=, ==, -, +, in (2), out (2);
+ * 
+ */
+
+public class OddEvenDetection {
+	public static void main(String[] args) {
+		int even = 7;
+		
+		if(even __ 2 __ 0) {
+			System.__.println("Even!")
+		}
+		else {
+			System.__.println("Odd!");
+		}
+	}
+}
