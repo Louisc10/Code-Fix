18150fc67d8995abdf70351fd4b2c6d25eb1b003
zzeordyga <zzeordyga@gmail.com>
2022-07-18 16:45:09 +0700

    adding fixes to drag and drop features

--- App/CodeFix/Assets/Scripts/Draggable.cs
+++ App/CodeFix/Assets/Scripts/Draggable.cs
@@ -7,25 +7,39 @@ public class Draggable : MonoBehaviour
     public bool IsDragging;
 
     public Vector3 LastPosition;
+    public Vector3 _initPosition;
 
     private Collider2D _collider;
+    private Collider2D _lastCollidedWith;
 
     private DragController dragController;
 
     private float movementTime = 15f;
     private System.Nullable<Vector3> movementDst;
 
-    private Draggable collideDraggable;
+    private bool isInit = false;
 
     void Start()
     {
         _collider = GetComponent<Collider2D>();
+
         dragController = FindObjectOfType<DragController>();
+        LastPosition = transform.position;
+        _initPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Still ineffecient
+        Vector2 size = ((RectTransform)transform).sizeDelta;
+        ((BoxCollider2D)_collider).size = new Vector2(size.x, size.y);
+        ((BoxCollider2D)_collider).offset = new Vector2(0, size.y / 3);
     }
 
     void FixedUpdate()
     {
-        if(movementDst.HasValue)
+
+        if (movementDst.HasValue)
         {
             if (IsDragging)
             {
@@ -44,36 +58,41 @@ public class Draggable : MonoBehaviour
             }
         }
 
-        if (dragController.LastDragged?.gameObject == gameObject)
+        if(!IsDragging && _lastCollidedWith == null)
         {
-            movementDst = LastPosition;
+            movementDst = _initPosition;
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        collideDraggable = other.GetComponent<Draggable>();
+        Draggable collideDraggable = other.GetComponent<Draggable>();
 
-        if(collideDraggable != null && dragController.LastDragged.gameObject == gameObject)
-        {
-            ColliderDistance2D colliderDistance2D = other.Distance(_collider);
-            Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y);
+        //if(collideDraggable != null && dragController.LastDragged.gameObject == gameObject)
+        //{
+        //    ColliderDistance2D colliderDistance2D = other.Distance(_collider);
+        //    Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y);
 
-            // Distance not applied for some reason
-            Debug.Log(diff);
-
-            transform.position -= diff;
-
-            collideDraggable = null;
-        }
+        //    transform.position -= diff;
+        //}
 
         if (other.CompareTag("DropValid"))
         {
+            _lastCollidedWith = other;
             movementDst = other.transform.position;
         }
         else
         {
             movementDst = LastPosition;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("DropValid"))
+        {
+            _lastCollidedWith = null;
+            movementDst = _initPosition;
         }
     }
 }
