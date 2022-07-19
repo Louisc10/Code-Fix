b2a7f93895dff706286606669bb899cfb8c86640
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2022-07-19 14:30:02 +0700

    Add Popup Window at Game Scene

--- App/CodeFix/Assets/Scripts/AnswerController.cs
+++ App/CodeFix/Assets/Scripts/AnswerController.cs
@@ -60,15 +60,53 @@ public class AnswerController : MonoBehaviour
             }
         }
         if(!flag){
-            Debug.Log("Masih ada yang salah");
+            isCorrect = true ;
+            labelMessage = "Sorry!! Please try again!!";
         }
         else{
-            Debug.Log("Sudah bener semua");
+            isCorrect = true;
+            labelMessage = "You fix the code!!! :)" ;
         }
+        OnGUI();
     }
 
     public void reset(GameObject question)
     {
         GetQuestion(question).isCorrect = false;
+    }
+
+    private string labelMessage;
+    private bool isCorrect;
+ 
+    void OnGUI()
+    {
+        if (isCorrect)
+        {
+            int windowWidth = 400;
+            int windowHeight = 300;
+
+            GUI.color = Color.white;
+            GUIStyle style = GUI.skin.GetStyle ("window"); 
+            style.fixedHeight = windowHeight;
+            style.fontSize = 30;
+            GUI.Window(0, new Rect((Screen.width / 2) - (windowWidth/2), (Screen.height / 2) - (windowHeight/2), windowWidth, windowHeight), showGUI, "", style);
+        }
+    }
+ 
+    void showGUI(int windowId)
+    {
+        GUIStyle style = GUI.skin.GetStyle ("label"); 
+        style.fontSize = 30;
+        if (isCorrect)
+        {
+            GUI.Label(new Rect(25, 40, 350, 200), labelMessage, style);
+        }
+ 
+        style = GUI.skin.GetStyle ("button"); 
+        style.fontSize = 30;
+        if (GUI.Button(new Rect(50, 200, 200, 50), "OK", style))
+        {
+            isCorrect = false;
+        }
     }
 }
